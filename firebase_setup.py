b28164b2db13198f84ad06c4225a57f9baba7c8c
bigkaiyoh <rbc_kaiyoh@gmail.com>
2024-07-22 19:56:42 +0900

    updated setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -1,14 +1,20 @@
+import json
+from google.cloud import firestore, secretmanager
 import firebase_admin
 from firebase_admin import credentials
-from google.cloud import secretmanager
 
-def get_secret(secret_id):
+def get_firebase_creds():
     client = secretmanager.SecretManagerServiceClient()
-    name = f"{secret_id}/versions/latest"
-    response = client.access_secret_version(name=name)
-    return response.payload.data.decode("UTF-8")
+    secret_name = "projects/581656499945/secrets/firebase-service-account-key/versions/latest"
+    response = client.access_secret_version(request={"name": secret_name})
+    secret_string = response.payload.data.decode("UTF-8")
+    return json.loads(secret_string)
 
-firebase_creds = get_secret("projects/581656499945/secrets/firebase-service-account-key")
+firebase_creds = get_firebase_creds()
 
+# Initialize Firebase Admin SDK with the credentials
 cred = credentials.Certificate(firebase_creds)
 firebase_admin.initialize_app(cred)
+
+# Initialize Firestore DB
+db = firestore.client()
