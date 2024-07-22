9700ae7a111d24616e93350ed5c0bf8af861c45d
bigkaiyoh <rbc_kaiyoh@gmail.com>
2024-07-22 20:07:22 +0900

    updated setup.py

--- firebase_setup.py
+++ firebase_setup.py
@@ -1,7 +1,7 @@
 import json
 from google.cloud import firestore, secretmanager
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, firestore
 
 def get_firebase_creds():
     client = secretmanager.SecretManagerServiceClient()
