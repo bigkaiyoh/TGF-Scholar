2bfdd65fc702174100d26b012e8020ea32ddc483
bigkaiyoh <rbc_kaiyoh@gmail.com>
2024-07-22 18:16:39 +0900

    adjusted for gcl

--- firebase_setup.py
+++ firebase_setup.py
@@ -1,9 +1,23 @@
+import os
 import firebase_admin
 from firebase_admin import credentials, firestore
+from google.cloud import firestore as google_firestore
 
-# Path to your Firebase service account key file
-cred = credentials.Certificate("unicke-firebase-adminsdk-l7s4j-d04bf19ccb.json")
-firebase_admin.initialize_app(cred)
+def initialize_firebase():
+    if os.getenv('unicke'):
+        # Running on Google Cloud, use Application Default Credentials
+        cred = credentials.ApplicationDefault()
+        firebase_admin.initialize_app(cred, {
+            'projectId': os.getenv('unicke'),
+        })
+        db = google_firestore.Client()
+    else:
+        # Local development, use service account key file
+        cred = credentials.Certificate("unicke-firebase-adminsdk-l7s4j-d04bf19ccb.json")
+        firebase_admin.initialize_app(cred)
+        db = firestore.client()
+    
+    return db
 
 # Initialize Firestore
-db = firestore.client()
+db = initialize_firebase()
